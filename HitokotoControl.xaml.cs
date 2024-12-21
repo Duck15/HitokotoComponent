f897a4f63c1956d19e67ef3b898c34a6d54b32af
2409 <95078036+Duck15@users.noreply.github.com>
2024-12-21 10:50:27 +0000

    添加定时器，每20分钟重置一次

--- HitokotoControl.xaml.cs
+++ HitokotoControl.xaml.cs
@@ -23,7 +23,20 @@ namespace HitokotoComponent
             InitializeComponent();
             LoadHitokotoAsync();
         }
+// 新建一个定时器，用于每20分钟执行一次LoadHitokotoAsync()函数
 
+// 创建一个定时器
+System.Threading.Timer timer = new System.Threading.Timer(
+    // 定时器触发时执行的回调函数
+    o => LoadHitokotoAsync(),
+    // 定时器触发时传递给回调函数的状态对象，这里不需要，所以传递null
+    null,
+    // 定时器首次触发前的延迟时间，这里设置为0，即立即执行
+    0,
+    // 定时器触发的间隔时间，这里设置为20分钟，即1200000毫秒
+    1200000); 
+
+// 这段代码创建了一个定时器，它会在程序启动时立即执行一次 `LoadHitokotoAsync()` 方法，并且之后每隔20分钟执行一次。
         private async void LoadHitokotoAsync()
         {
             try
